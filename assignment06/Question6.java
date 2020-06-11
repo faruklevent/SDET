1a3eb4aa2095d0bf003f0ecaddf8d058f75f1e05
faruklevent <65966892+faruklevent@users.noreply.github.com>
2020-06-11 16:53:35 +0300

    Update Question6.java

--- assignment06/Question6.java
+++ assignment06/Question6.java
@@ -4,10 +4,10 @@ public class Question6 {
 
 	public static void main(String[] args) {
 	/*  Write a Java program to print the following pattern
-		1				print row#
-		2 6				print row# and row#+4
-		3 7 10			print row# and row#+4 and row#+4+3
-		4 8 11 13		print row# and row#+4 and row#+4+3 and row#+4+3+2
+		1		print row#
+		2 6		print row# and row#+4
+		3 7 10		print row# and row#+4 and row#+4+3
+		4 8 11 13	print row# and row#+4 and row#+4+3 and row#+4+3+2
 		5 9 12 14 15	print row# and row#+4 and row#+4+3 and row#+4+3+2 and row#+4+3+2+1
 	*/
 
