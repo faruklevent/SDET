e91bd3f6e4cfa6f5b69bffe239b12e0f031af137
faruklevent <65966892+faruklevent@users.noreply.github.com>
2020-06-11 16:50:56 +0300

    Update Question3.java

--- assignment06/Question3.java
+++ assignment06/Question3.java
@@ -6,15 +6,15 @@ public class Question3 {
 	/*	Write a Java program to print the following pattern
 		1 2 3 4 5 6 7	//row1  0 spaces + 1-7  ==> number of spaces: row-1,  numbers: from row till 7
 		 2 3 4 5 6 7	//row2  1 space  + 2-7  ==> same above
-		  3 4 5 6 7		//row3  2 spaces + 3-7  ==> same above
-		   4 5 6 7		//row4  3 spaces + 4-7  ==> same above
-		    5 6 7		//row5  4 spaces + 5-7  ==> same above
-		     6 7		//row6  5 spaces + 6-7  ==> same above
-		      7			//row7  6 spaces + 7-7  ==> same above
-		     6 7		//row8  5 spaces + 6-7  ==> number of spaces: 6-row%7,  numbers:from 7-row%7 till 7
-		    5 6 7		//row9  4 spaces + 5-7  ==> same above
-		   4 5 6 7		//row10 3 spaces + 4-7  ==> same above  
-		  3 4 5 6 7		//row11 2 spaces + 3-7  ==> same above
+		  3 4 5 6 7	//row3  2 spaces + 3-7  ==> same above
+		   4 5 6 7	//row4  3 spaces + 4-7  ==> same above
+		    5 6 7	//row5  4 spaces + 5-7  ==> same above
+		     6 7	//row6  5 spaces + 6-7  ==> same above
+		      7		//row7  6 spaces + 7-7  ==> same above
+		     6 7	//row8  5 spaces + 6-7  ==> number of spaces: 6-row%7,  numbers:from 7-row%7 till 7
+		    5 6 7	//row9  4 spaces + 5-7  ==> same above
+		   4 5 6 7	//row10 3 spaces + 4-7  ==> same above  
+		  3 4 5 6 7	//row11 2 spaces + 3-7  ==> same above
 		 2 3 4 5 6 7	//row12 1 space  + 2-7  ==> same above
 		1 2 3 4 5 6 7   //row13 0 spaces + 1-7  ==> same above
 	*/
